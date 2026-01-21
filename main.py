16cae1b0613d53d4e6a6e547e377f141a4c91abb
jlry-dev <personal.jlry@gmail.com>
2026-01-21 22:00:59 +0800

    feat: added division function

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def subtraction(a, b):
 
 def multiplication (a, b):
     return a * b
+
+def division(dividend, divisor):
+    if divisor == 0:
+        return "Error: Cannot divide by zero"
+    return dividend / divisor
+
