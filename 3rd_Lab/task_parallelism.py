4eb22290d383eff3ef1427a9806131ceb4a11322
jlry-dev <personal.jlry@gmail.com>
2026-02-13 22:18:45 +0800

    task: added main function (made ebreting work)

--- 3rd_Lab/task_parallelism.py
+++ 3rd_Lab/task_parallelism.py
@@ -27,3 +27,43 @@ def compute_tax(salary):
     amount = salary * 0.10
     print(f"[{thread}] Withholding Tax computed")
     return amount
+
+employees = [
+    ("Alice", 25000),
+    ("Bob", 32000),
+    ("Charlie", 28000),
+    ("Diana", 40000),
+    ("Edward", 35000)
+]
+
+def main():
+    print("\n===== PART A: TASK PARALLELISM =====\n")
+
+    for name, salary in employees:
+        print(f"\nEmployee: {name}")
+        print(f"Salary: ₱{salary:,.2f}")
+
+        with ThreadPoolExecutor(max_workers=4) as executor:
+
+            f_sss = executor.submit(compute_sss, salary)
+            f_ph = executor.submit(compute_philhealth, salary)
+            f_pi = executor.submit(compute_pagibig, salary)
+            f_tax = executor.submit(compute_tax, salary)
+
+            sss = f_sss.result()
+            philhealth = f_ph.result()
+            pagibig = f_pi.result()
+            tax = f_tax.result()
+
+        total = sss + philhealth + pagibig + tax
+
+        print("\n--- Deductions ---")
+        print(f"SSS: ₱{sss:,.2f}")
+        print(f"PhilHealth: ₱{philhealth:,.2f}")
+        print(f"Pag-IBIG: ₱{pagibig:,.2f}")
+        print(f"Withholding Tax: ₱{tax:,.2f}")
+        print("----------------------")
+        print(f"TOTAL DEDUCTION: ₱{total:,.2f}")
+
+if __name__ == "__main__":
+    main()
