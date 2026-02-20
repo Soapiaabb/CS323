ffc1c3a64ae573ba1f405a90729db4573f6e343a
Soapiaabb <Villarente.sofiabelle027@gmail.com>
2026-02-20 23:42:29 +0800

    feat: added request generator

--- 2nd_Group_Activity/activity.py
+++ 2nd_Group_Activity/activity.py
@@ -1,6 +1,11 @@
 import threading
+import random
 
 # Simulated shared resources
 customer_db = {}
 db_lock = threading.Lock()
 cooking_lock = threading.Lock()
+
+def generate_requests(n=20):
+    types = ["time", "payment", "food", "support"]
+    return [(i, random.choice(types)) for i in range(n)]
