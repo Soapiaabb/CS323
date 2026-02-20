dee2a8fc2b1456d953d2386bca3a330f918edd4f
Markx231 <usmanmarkjason033@gmail.com>
2026-02-20 23:50:23 +0800

    feat: added request processor

--- 2nd_Group_Activity/activity.py
+++ 2nd_Group_Activity/activity.py
@@ -1,5 +1,6 @@
 import threading
 import random
+import time
 
 # Simulated shared resources
 customer_db = {}
@@ -9,3 +10,18 @@ cooking_lock = threading.Lock()
 def generate_requests(n=20):
     types = ["time", "payment", "food", "support"]
     return [(i, random.choice(types)) for i in range(n)]
+
+def process_request(request):
+    customer_id, req_type = request
+
+    if req_type in ["time", "payment"]:
+        time.sleep(0.1)
+        with db_lock:
+            customer_db[customer_id] = "updated"
+
+    elif req_type == "food":
+        with cooking_lock:
+            time.sleep(0.3)
+
+    elif req_type == "support":
+        time.sleep(0.2)
